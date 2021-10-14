e849f4f040c1c302bbf4a7789d66dd6d4c23faf0
Ashok Sekar <imashok02@mail.com>
2021-10-14 11:03:10 +0530

    location township(sublocation) migrations and price_qty module name change

--- migrations/20211014052811-sublocation-enabling-migration.js
+++ migrations/20211014052811-sublocation-enabling-migration.js
@@ -2,9 +2,10 @@
 
 exports.up = function(db, callback) {
   db.runSql("INSERT INTO `core_modules` (`module_id`, `module_name`, `module_desc`, `module_lang_key`, `module_icon`, `ordering`, `is_show_on_menu`, `group_id`) "+
-  	"VALUES (49, 'price_quantity', 'Price Quantity', 'price_qty_module', '', 20, 1, 1)", callback);
+  	"VALUES (50, 'item_location_townships', 'Location Townships', 'location_townships', '', 15, 1, 1)", callback);
 };
 
 exports.down = function(db, callback) {
-  db.runSql('DELETE FROM core_modules WHERE module_name = "pricequantity";', callback);
+  db.runSql('DELETE FROM core_modules WHERE module_name = "item_location_townships";', callback);
 };
+
